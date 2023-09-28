6e5b615b4ebd933094b620eb2eb02d04c938e1d5
harish <kopanathiharris@gmail.com>
2023-09-28 21:17:55 +0530

    modified

--- src/Nav.js
+++ src/Nav.js
@@ -3,15 +3,17 @@ import "./Nav.css";
 
 function Nav() {
   return (
-    <div className="nav">
+    <div className="nav nav_black">
+      <div className="nav_contents">
       <img 
-      className='nav_logo'  src="" alt=""  
-      />
-      <h1>clear </h1>
-      <ul>
-        <li>content</li>
-        <li>clear</li>
-      </ul>
+      className='nav_logo'  
+      src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+      alt=""/>
+      <img 
+      className='nav_avatar'
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+      alt=""/>
+      </div>
     </div>
   );
 }
