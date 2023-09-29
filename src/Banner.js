ab630d6f8400f4f0cebc76d34870a0e275bc9b52
harish <kopanathiharris@gmail.com>
2023-09-29 18:38:20 +0530

    modified

--- src/Banner.js
+++ src/Banner.js
@@ -3,6 +3,10 @@ import './Banner.css';
 
 
 function Banner() {
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + '...' : string; 
+  }
   return (
     <header
       className="banner" 
