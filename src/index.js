db903f3dabb28adec94c2b31f8475929971597eb
harish <kopanathiharris@gmail.com>
2023-10-15 00:02:01 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -18,8 +18,6 @@ root.render(
   </React.StrictMode>
 );
 // CLEAR
-// clefnfd
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
