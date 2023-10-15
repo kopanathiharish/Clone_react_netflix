5301feaae00ddcaa0bf6b9ab634f30b490fc10a5
harish <kopanathiharris@gmail.com>
2023-10-16 00:01:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-// CLEAR
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
