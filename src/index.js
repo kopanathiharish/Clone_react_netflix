ee5d0c074c9f4707cf7ecf3c571168f671176a14
harish <kopanathiharris@gmail.com>
2024-02-02 19:00:00 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ root.render(
   </React.StrictMode>
 );
 
-#fsdfs#
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
