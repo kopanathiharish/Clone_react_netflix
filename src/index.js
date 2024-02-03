7eb893e5e339cc522071101d4c890567e974cd9b
harish <kopanathiharris@gmail.com>
2024-02-03 09:55:39 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ root.render(
 
 
 //learn form stau o
-// learn to undetstantsfnsdj f-f kj
+// learn to understand
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
