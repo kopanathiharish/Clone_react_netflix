78755536a8744731a77d9451cf0261b629f8c7a5
harish <kopanathiharris@gmail.com>
2024-02-06 19:42:29 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -26,4 +26,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+\
+sfs
+fsf
+sfd
 
